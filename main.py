8e9ecadfd65eb2f3a87161e53da3994e775d7ce5
Lammy85 <daniela.sachs@yahoo.de>
2024-11-18 13:36:15 +0100

    sql

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from datetime import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['WTF_CSRF_ENABLED'] = False
 
 db = SQLAlchemy(app)
 
@@ -13,22 +14,27 @@ class Highscore(db.Model):
     playername = db.Column(db.String(200), nullable=True)
     totalScore = db.Column(db.Integer, nullable=True)
 
-    @app.route('/', methods=['GET', 'POST'])
-    def start_page():
-
-        if request.method == 'POST':
-
-            new_highscore = Highscore(
-                playername = request.form['playername'],
-                totalScore = request.form['totalScore']            
-            )
-            db.session.add(new_highscore)
-            db.session.commit()
-
-            return redirect('/')
+@app.route('/', methods=['GET', 'POST'])
+def start_page():           
         
-        daten = Highscore.query.all()
-        return render_template('index.html', daten=daten)
+    daten = Highscore.query.all()
+    return render_template('index.html', daten=daten)
+    
+@app.route('/saveScore', methods=['GET', 'POST'])
+def saveScore():
+    if request.method == 'POST':
+    
+        data = request.get_json()
+        wert = data.get('totalScore')
+
+        new_highscore = Highscore(
+            playername = request.form['playername'],
+            totalScore = wert
+        )
+        db.session.add(new_highscore)
+        db.session.commit()
+        return redirect(url_for('index.html'))
+        
     
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
