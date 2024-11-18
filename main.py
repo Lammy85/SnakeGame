d5e4a2ff3ac6bea506116eb7cf026c01b17785f5
Lammy85 <daniela.sachs@yahoo.de>
2024-11-18 16:24:46 +0100

    feierabend

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.config['WTF_CSRF_ENABLED'] = False
 
 db = SQLAlchemy(app)
+CORS(app)
+
 
 class Highscore(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    playername = db.Column(db.String(200), nullable=True)
-    totalScore = db.Column(db.Integer, nullable=True)
+    playername = db.Column(db.String(200), nullable=False)
+    totalScore = db.Column(db.Integer, nullable=False)
 
 @app.route('/', methods=['GET', 'POST'])
 def start_page():           
@@ -20,20 +22,20 @@ def start_page():
     daten = Highscore.query.all()
     return render_template('index.html', daten=daten)
     
-@app.route('/saveScore', methods=['GET', 'POST'])
-def saveScore():
-    if request.method == 'POST':
-    
-        data = request.get_json()
-        wert = data.get('totalScore')
+@app.route('/saveScore', methods=['POST'])
 
-        new_highscore = Highscore(
-            playername = request.form['playername'],
-            totalScore = wert
-        )
-        db.session.add(new_highscore)
-        db.session.commit()
-        return redirect(url_for('index.html'))
+def save_score():
+    
+    data = request.get_json()
+    
+    playername = data.get('playername')
+    total_score = data.get('totalScore')
+        
+    new_highscore = Highscore(playername=playername, totalScore=total_score)
+    
+    db.session.add(new_highscore)
+    db.session.commit()
+    return redirect(url_for('index.html'))
         
     
 if __name__ == "__main__":
