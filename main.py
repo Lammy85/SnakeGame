389757822b169bbcf2933cb7dabd95355cbadf34
Lammy85 <daniela.sachs@yahoo.de>
2024-11-20 11:08:39 +0100

    comments

--- main.py
+++ main.py
@@ -9,33 +9,48 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 CORS(app)
 
-
 class Highscore(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     playername = db.Column(db.String(200), nullable=False)
     totalScore = db.Column(db.Integer, nullable=False)
 
 @app.route('/', methods=['GET', 'POST'])
-def start_page():           
+def start_page():
+
+    #gets the data for the highscore table           
         
     daten = Highscore.query.order_by(Highscore.totalScore.desc()).all()
+
+    #Top ten -> we only want the first 10 entries
+
     top_daten = daten[:10]
     return render_template('index.html', daten=top_daten)
+
+#endpoint for score saving
     
 @app.route('/saveScore', methods=['POST'])
 
 def save_score():
+
+    #receives the data sent with sendData()
     
     data = request.get_json()
     
     playername = data.get('playername')
     total_score = data.get('totalScore')
+
+    #generating a new Highscore objekt with the data
         
     new_highscore = Highscore(playername=playername, totalScore=total_score)
+
+    #saving the objekt in the database
     
     db.session.add(new_highscore)
     db.session.commit()
-    return jsonify({"message": "Daten erfolgreich gespeichert"})
+
+    #return as json important!
+
+    return jsonify({"message": "Data successfully saved"})
 
     
 if __name__ == "__main__":
